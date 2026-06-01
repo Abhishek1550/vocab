a358c59f606356c545b07a79dd743c9ef86a79e0
abhishekborse5015 <abhishekborse5015@gmail.com>
2026-06-01 09:59:44 +0530

    fix: Name should be uniuqe per user

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 class Domain(models.Model):
     """Main category like Kitchen, Travel, Medical, Business etc."""
-    name = models.CharField(max_length=100, unique=True, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
